6c1667c94658ac63f3d8a43810b290c86b4cc990
Nahshon Unna Tsameret <nahsh.ut@gmail.com>
2026-02-16 08:27:47 +0200

    fix the release script
    Signed-off-by: Nahshon Unna Tsameret <nahsh.ut@gmail.com>

--- cmd/ptr_to_new/cmd.go
+++ cmd/ptr_to_new/cmd.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"runtime"
@@ -8,12 +9,14 @@ import (
 	"golang.org/x/tools/go/analysis/singlechecker"
 
 	"ptrtonew"
-	"ptrtonew/myversion"
 )
+
+//go:embed version.txt
+var version string
 
 func main() {
 	if len(os.Args) == 2 && os.Args[1] == "version" {
-		fmt.Printf("ptrToNew version: %s\n", myversion.Version)
+		fmt.Printf("ptrToNew version: %s\n", version)
 		fmt.Printf("go version:       %s\n", runtime.Version())
 		os.Exit(0)
 	}
